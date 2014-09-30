a5f6a6ae61e99f0f3aace9e6ceb2edc35fe3b4aa
rajneeshsharma81 <rajneeshsharmakota@gmail.com>
2014-09-30 18:00:09 +0530

    Updated files

---
+++ AngularJSDemo/AngularJSDemo/Controllers/LogItemsController.cs
@@ -0,0 +1,111 @@
+﻿using AngularJSDemo.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace AngularJSDemo.Controllers
+{
+    public class LogItemsController : ODataController
+    {
+        LogItemsContext db = new LogItemsContext();
+
+
+        [EnableQuery]
+        public List<LogItem> Get(int? pageNumber, int? pageSize, string sortInfo, string name)
+        {
+            //Skip elements
+            int skip = 0;
+            if (pageNumber >1 )
+            {
+                skip = (pageNumber.Value - 1) * pageSize.Value;
+            }
+                        
+            if (sortInfo != null)
+            {
+                name = name == null ? "" : name;
+                
+                switch (sortInfo)
+                {
+                    case "+LogLevel":
+                        var lstRec = (from c in db.LogItems
+                                  where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                   select c).OrderBy(d => d.LogLevel).Skip(skip).Take(pageSize.Value).ToList();
+                        return lstRec;
+                        break;
+                    case "-LogLevel":
+                        var lstRec1 = (from c in db.LogItems
+                                      where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                       select c).OrderByDescending(d => d.LogLevel).Skip(skip).Take(pageSize.Value).ToList();
+                        return lstRec1;
+                        break;
+                    case "-UserName":
+                        var lstRec2 = (from c in db.LogItems
+                                      where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                       select c).OrderByDescending(d => d.UserName).Skip(skip).Take(pageSize.Value).ToList();                        
+                        return lstRec2;
+                        break;
+                    case "+UserName":
+                        var lstRec3 = (from c in db.LogItems
+                                       where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                       select c).OrderBy(d => d.UserName).Skip(skip).Take(pageSize.Value).ToList();
+                        return lstRec3;
+                        break;
+                    case "+Module":
+                        var lstRec4 = (from c in db.LogItems
+                                       where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                       select c).OrderBy(d => d.Module).Skip(skip).Take(pageSize.Value).ToList();
+                        return lstRec4;
+                        break;
+                    case "-Module":
+                        var lstRec5 = (from c in db.LogItems
+                                       where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                       select c).OrderByDescending(d => d.Module).Skip(skip).Take(pageSize.Value).ToList();
+                        return lstRec5;
+                        break;
+
+                    case "+Id":
+                        var lstRec6 = (from c in db.LogItems
+                                       where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                       select c).OrderBy(d => d.Id).Skip(skip).Take(pageSize.Value).ToList();
+                        return lstRec6;
+                        break;
+                    case "-Id":
+                        var lstRec7 = (from c in db.LogItems
+                                       where c.LogLevel.Contains(name) || c.UserName.Contains(name)
+                                       select c).OrderByDescending(d => d.Id).Skip(skip).Take(pageSize.Value).ToList();
+                        return lstRec7;
+                        break;
+                }
+            }
+            List<LogItem> lstFiltered = db.LogItems.OrderBy(d => d.Id).Skip(skip).Take(pageSize.Value).ToList();
+            return lstFiltered;
+        }
+
+        [EnableQuery]
+        public SingleResult<LogItem> Get([FromODataUri] int key)
+        {
+            IQueryable<LogItem> result = db.LogItems.Where(p => p.Id == key);
+            return SingleResult.Create(result);
+        }
+
+        private bool LogItemExists(int key)
+        {
+            return db.LogItems.Any(p => p.Id == key);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AngularJSDemo/AngularJSDemo/Scripts/Controllers/NGController.js
@@ -0,0 +1,154 @@
+﻿
+var app = angular.module('myApp', ['ngGrid']);
+
+app.controller('MyCtrl', ['$scope', '$http', function ($scope, $http, $apply) {
+    $scope.items = [];
+    $scope.SortDirection = "-";
+    $scope.filterText = "";
+    
+    // filter
+    $scope.filterOptions = {
+        filterText: "",
+        useExternalFilter: false
+    };
+
+    // paging
+    $scope.totalServerItems = 0;
+    $scope.pagingOptions = {
+        pageSizes: [5,10,25, 50, 100],
+        pageSize: 5,
+        currentPage: 1
+    };
+
+    // sort
+    $scope.sortOptions =
+        {
+        fields: ["Id"],
+        directions: ["asc"]
+    };
+
+    // grid
+    $scope.gridOptions =
+        {
+        data: "items",
+        columnDefs: [
+            { field: "Id", displayName: "ID", width: "10%" },
+            { field: "Module", displayName: "Module", width: "30%" },
+            { field: "LogLevel", displayName: "Log Level", width: "30%" },
+            { field: "UserName", displayName: "Username", width: "20%" },
+            { displayName: 'Actions', cellTemplate: 
+             '<div class="grid-action-cell">'+
+             '<a ng-click="$event.stopPropagation(); ShowDetails(row.entity.Id);" href="#">Details</a></div>'
+            }
+            
+        ],
+            
+        enablePaging: true,
+        enablePinning: false,
+        pagingOptions: $scope.pagingOptions,
+        //filterOptions: $scope.filterOptions,
+        keepLastSelected: true,
+        multiSelect: false,
+        showColumnMenu: false,
+        //showFilter: true,
+        showGroupPanel: false,
+        showFooter: true,
+        sortInfo: $scope.sortOptions,
+        totalServerItems: "totalServerItems",
+        useExternalSorting: true,
+        enableSorting: true,
+        i18n: "en"
+    };
+
+
+    $scope.ShowDetails= function (ID)
+    {
+        $http({
+            url: "/odata/LogItems("+ID+")",
+            method: "GET"
+        }).success(function (data, status, headers, config)
+        {
+            $('#myModal').modal({
+                show: 'true'
+            });
+            $scope.Module = data.Module;
+            $scope.LoggedOn = data.LoggedOn;
+            $scope.LogLevel = data.LogLevel;
+            $scope.UserName = data.UserName;
+            $scope.Message = data.Message;
+
+        }).error(function (data, status, headers, config) {
+            alert(JSON.stringify(data));
+        });
+
+    };
+
+    $scope.selectGridRow = function () {
+        if ($scope.selectedItem[0].total != 0) {
+            $location.path('items/' + $scope.selecteditem[0].id);
+        }
+    };
+
+    $scope.refresh = function ()
+    {
+        var sb = [];
+        for (var i = 0; i < $scope.sortOptions.fields.length; i++)
+        {
+            sb.push($scope.sortOptions.directions[i] === "desc" ? "+" : "-");
+            sb.push($scope.sortOptions.fields[i]);
+        }
+        
+        var p =
+            {
+                name: $scope.filterText,
+                pageNumber: $scope.pagingOptions.currentPage,
+                pageSize: $scope.pagingOptions.pageSize,
+                sortInfo: sb.join("")
+            };
+
+        $http({
+            url: "/odata/LogItems",
+            method: "GET",
+            params: p
+        }).success(function (data, status, headers, config) {
+            $scope.totalServerItems = data.totalItems;
+            $scope.items = data.value;
+
+        }).error(function (data, status, headers, config) {
+            alert(JSON.stringify(data));
+        });
+
+        setTimeout(function ()
+        {
+            
+        }, 100);
+    };
+
+    // watches
+    $scope.$watch('pagingOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal)
+        {
+            
+            $scope.refresh();
+        }
+    }, true);
+
+    $scope.$watch('filterOptions', function (newVal, oldVal)
+    {
+        if (newVal !== oldVal)
+        {
+            
+            $scope.refresh();
+        }
+    }, true);
+
+    $scope.$watch('sortOptions', function (newVal, oldVal)
+    {
+        if (newVal !== oldVal)
+        {
+            $scope.refresh();
+        }
+    }, true);
+
+    $scope.refresh();
+}]);
